ce0571ba56b5180c9618f2e2f5dec64761fd4fe2
yang dong won <didehddnjs89@gmail.com>
2024-04-30 18:39:23 +0900

    RoomDatabase

---
+++ app/src/main/java/com/example/room_prac/Student.kt
@@ -0,0 +1,11 @@
+package com.example.room_prac
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "student_table")    // 테이블 이름을 student_table로 지정함
+data class Student (
+    @PrimaryKey @ColumnInfo(name = "student_id") val id: Int,
+    val name: String
+)
